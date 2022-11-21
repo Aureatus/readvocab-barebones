39117f22aaf5ab43485bb127c3f12487256fc42b
Alex Knott <alextknott@gmail.com>
2022-11-21 17:01:37 +0000

    Create function that gets list of words from a pdf file

--- javascriptImplementation/functions/helpers.js
+++ javascriptImplementation/functions/helpers.js
@@ -1,5 +1,28 @@
+import pkg from "pdfjs-dist";
+const { getDocument } = pkg;
+
 const punctuationFilter = (wordList) => {
   const invalidCharRegex = /([^\s\w])/g;
 
   return wordList.replaceAll(invalidCharRegex, " ");
 };
+
+const getWords = async (dir) => {
+  const doc = await getDocument(dir).promise;
+  const docPages = doc.numPages;
+  let text = "";
+  for (let i = 1; i <= docPages; i++) {
+    const page = await doc.getPage(i);
+    const pageInfo = await page.getTextContent();
+    const pageText = pageInfo.items.map((item) => item.str);
+    text = text.concat(pageText);
+  }
+
+  const normalizedText = punctuationFilter(text).trim().replaceAll(/  +/g, " ");
+  const words = normalizedText.split(" ");
+
+  const trimmedWords = words.map((word) => word.trim());
+  return trimmedWords;
+};
+
+export { getWords };
